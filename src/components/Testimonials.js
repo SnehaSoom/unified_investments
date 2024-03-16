012f3eb400346c1b05b092b04341896c21488a4a
Sneha Soom <soomsneha05@gmail.com>
2024-03-16 14:01:31 +0530

    complete

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -18,19 +18,6 @@ const testimonials = [
       "Is be upon sang fond must shew. Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now.",
     image2: johnImage,
   },
-
-  // {
-  //   name: "Danial H",
-  //   role: "CEO GetNextDesign",
-  //   text: "Is be upon sang fond must shew. Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now.",
-  //   image: danialImage,
-  // },
-  // {
-  //   name: "John H",
-  //   role: "Design",
-  //   text: "Is be upon sang fond must shew. Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now.",
-  //   image: johnImage,
-  // },
 ];
 
 const Testimonials = () => {
